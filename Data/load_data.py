e8e906d3caf517b607b2e29204842f86d6a18a78
Ansh Sharma <as20482@log-3.hpc.nyu.edu>
2024-11-28 05:53:20 -0500

    Fix parallelisation bug in data loading and make onehot labels + some analysis code

--- Data/load_data.py
+++ Data/load_data.py
@@ -8,37 +8,35 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 DATA_PATH = '/scratch/as20482/ML_Final_Proj/audioset-20k-wds/balanced'
 OUTPUT_PATH = '/scratch/as20482/ML_Final_Proj/AudioSet-classification/Data'
-target_sample_rate = 16000
-origin_sample_rate = 32000
+TARGET_SAMPLE_RATE = 16000
+ORIGIN_SAMPLE_RATE = 32000
+NUM_CLASSES = 527
 
 
 def load_and_resample_waveform(file_path):
     waveform, original_sample_rate = torchaudio.load(file_path)
-    assert original_sample_rate == origin_sample_rate
-    resample_transform = torchaudio.transforms.Resample(orig_freq=original_sample_rate, new_freq=target_sample_rate)
-    return resample_transform(waveform)
+    assert original_sample_rate == ORIGIN_SAMPLE_RATE
+    resample_transform = torchaudio.transforms.Resample(orig_freq=original_sample_rate, new_freq=TARGET_SAMPLE_RATE)
+    return (file_path, resample_transform(waveform))
 
 def pad_or_clip_waveform(waveform, target_length):
+    path, waveform = waveform
     current_length = waveform.shape[1]
     if current_length < target_length:
         padding = (0, target_length - current_length)
         waveform = F.pad(waveform, padding)
     else:
         waveform = waveform[:, :target_length]
-    return waveform
-
-def pad_label(label, target_length):
-    return label + [-1] * (target_length - len(label))
+    return (path, waveform)
 
 def load_and_index(split = 'test'):
     wav_files = [f for f in os.listdir(os.path.join(DATA_PATH, split)) if f.endswith('.wav')]
-    wav_files.sort()
     return wav_files
 
 def load_label_from_json(file_path):
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
-        return data["label"]
+        return (file_path, data["label"])
 
 def save_16khz_audio(split = 'test'):
     wav_files = load_and_index(split)
@@ -50,8 +48,8 @@ def save_16khz_audio(split = 'test'):
             resampled_waveforms.append(future.result())
     
     assert len(resampled_waveforms) == len(wav_files)
-
-    max_length = max(waveform.shape[1] for waveform in resampled_waveforms)
+    
+    max_length = max(waveform[1].shape[1] for waveform in resampled_waveforms)
 
     padded_waveforms = []
     with ThreadPoolExecutor() as executor:
@@ -61,7 +59,9 @@ def save_16khz_audio(split = 'test'):
 
     assert len(padded_waveforms) == len(wav_files)
 
-    batch_waveforms_tensor = torch.stack(padded_waveforms)
+    padded_waveforms.sort()
+    
+    batch_waveforms_tensor = torch.stack(list(map(lambda x: x[1], padded_waveforms)))
 
     torch.save(batch_waveforms_tensor, os.path.join(OUTPUT_PATH, split, 'resampled_waveforms.pt'))
 
@@ -72,17 +72,17 @@ def save_16khz_audio(split = 'test'):
         futures = {executor.submit(load_label_from_json, json_file): json_file for json_file in json_files}
         for future in tqdm(as_completed(futures), total=len(futures), desc="Loading Labels"):
             labels.append(future.result())
+
+    labels.sort()
     
-    max_length = max(len(label) for label in labels)
+    indices = [torch.tensor(label[1], dtype=torch.long) for label in labels]
+    onehot_labels= torch.zeros((len(labels), NUM_CLASSES), dtype=torch.long)
+    for i, index in enumerate(indices):
+        onehot_labels[i].scatter_(0, index, 1.0)
 
-    padded_labels = []
-    with ThreadPoolExecutor() as executor:
-        futures = {executor.submit(pad_label, label, max_length): i for i, label in enumerate(labels)}
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Padding Labels"):
-            padded_labels.append(future.result())
+    torch.save(onehot_labels, os.path.join(OUTPUT_PATH, split, 'labels.pt'))
 
-    labels_tensor = torch.tensor(padded_labels)
-    torch.save(labels_tensor, os.path.join(OUTPUT_PATH, split, 'labels.pt'))
+    assert list(map(lambda x: x[0].replace(".wav", ""), padded_waveforms)) == list(map(lambda x: x[0].replace(".json", ""), labels))
 
 
 
